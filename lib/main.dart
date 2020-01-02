f22eff933c269de50d80a26b2f4568c779d83de5
smrz <mr.razavi77@gmail.com>
2020-01-02 08:47:48 +0330

    routing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
 import 'pages/managment.dart';
 
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Test('مدیریت'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyHome(),
+        '/acceptance': (context) => Test('مدیریت'),
+//        '/addNew': (context) => MyHomePage(),
+      },
+    ));
+
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('مدیریت فروشگاه')),
+      body: Center(
+        child: Text('test'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('مدیریت فروشگاه'),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              title: Text('تایید صلاحیت'),
+              onTap: () {
+                Navigator.pushNamed(context, '/acceptance');
+//                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('افزودن کالا'),
+              onTap: () {
+                Navigator.pushNamed(context, '/addNew');
+//                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
+
