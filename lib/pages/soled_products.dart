ea464d81c6eed89fb19b8eb62f7c97b9613f60e7
HosseinAsadi76 <49914846+HosseinAsadi76@users.noreply.github.com>
2020-01-04 10:25:41 +0330

    modifying

--- lib/pages/soled_products.dart
+++ lib/pages/soled_products.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class SoledProducts extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+
+    );
   }
 }
