ba9d796f57e715c75544293965739bbc36e3886a
sara <s.moeini1377@gmail.com>
2020-01-31 23:00:23 +0330

    addproduct final

--- lib/pages/add_product.dart
+++ lib/pages/add_product.dart
@@ -1,17 +1,13 @@
+import 'dart:collection';
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import '../model/addproduct.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class AddProduct extends StatefulWidget {
   AddProduct({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -20,11 +16,33 @@ class AddProduct extends StatefulWidget {
 
 class _AddProductState extends State<AddProduct> {
   final _formKey = GlobalKey<FormState>();
-  bool _lights = false;
+  final _addProduct = AddProductModel();
+  final HashMap<String, String> map = new HashMap<String, String>();
+  File image;
+  File _image;
+  String h;
   String dropdownValue = 'آرایشی و بهداشت';
 
   @override
   Widget build(BuildContext context) {
+    Future getImage() async {
+      image= await ImagePicker.pickImage(source: ImageSource.gallery);
+      String base64Image = base64Encode(image.readAsBytesSync());
+      print(base64Image.length);
+      setState(() {
+        _addProduct.productImages=[{"imageContent":base64Image}];
+      });
+    }
+
+//    Future uploadPic(BuildContext context) async {
+//      setState(() {
+//        print("Profile Picture uploaded");
+//        Scaffold.of(context)
+//            .showSnackBar(SnackBar(content: Text('Profile Picture Uploaded')));
+//      }
+//      );
+//    }
+
     return Scaffold(
         backgroundColor: Color(0xffE0E0E0),
         appBar: AppBar(
@@ -50,16 +68,32 @@ class _AddProductState extends State<AddProduct> {
                               textDirection: TextDirection.rtl,
                               child: TextFormField(
                                   decoration: InputDecoration(
-                                labelText: 'نام محصول',
-                              )))),
+                                    labelText: 'نام محصول',
+                                  ),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'لطفا نام محصول را وارد کنید';
+                                    }
+                                  },
+                                  onSaved: (val) =>
+                                      setState(() => _addProduct.name = val)
+                              ))),
+
                       Padding(
                           padding: EdgeInsets.all(12),
                           child: new Directionality(
                               textDirection: TextDirection.rtl,
                               child: TextFormField(
                                 decoration: InputDecoration(labelText: 'قیمت'),
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'لطفا قیمت را وارد کنید';
+                                  }
+                                },
+                                onSaved: (val) => setState(
+                                        () => _addProduct.price = int.parse(val)),
                                 scrollPadding:
-                                    EdgeInsets.fromLTRB(12, 12, 12, 12),
+                                EdgeInsets.fromLTRB(12, 12, 12, 12),
                               ))),
                       Padding(
                           padding: EdgeInsets.all(12),
@@ -81,6 +115,7 @@ class _AddProductState extends State<AddProduct> {
                                 onChanged: (String newValue) {
                                   setState(() {
                                     dropdownValue = newValue;
+                                    _addProduct.category=newValue;
                                   });
                                 },
                                 items: <String>[
@@ -101,39 +136,119 @@ class _AddProductState extends State<AddProduct> {
                           child: new Directionality(
                               textDirection: TextDirection.rtl,
                               child: TextFormField(
-                                decoration: InputDecoration(labelText: 'تعداد'),
+                                  decoration:
+                                  InputDecoration(labelText: 'تعداد'),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'لطفا تعداد را وارد کنید';
+                                    }
+                                  },
+                                  onSaved: (val) => setState(
+                                          () => _addProduct.count = int.parse(val))
                               ))),
-//                      Padding(
-//                          padding: EdgeInsets.all(12),
-//                          child: new Directionality(
-//                              textDirection: TextDirection.rtl,
-//                              child: TextFormField(
-//                                decoration:
-//                                    InputDecoration(labelText: 'دسته بندی'),
-//                              ))),
+
                       Padding(
                           padding: EdgeInsets.all(12),
                           child: new Directionality(
                               textDirection: TextDirection.rtl,
                               child: TextFormField(
-                                decoration: InputDecoration(labelText: 'رنگ'),
+                                  decoration: InputDecoration(labelText: 'رنگ'),
+                                  onSaved: (val) => setState(
+                                          () => _addProduct.color = val)
                               ))),
+
                       Padding(
                           padding: EdgeInsets.all(12),
                           child: new Directionality(
                               textDirection: TextDirection.rtl,
                               child: TextFormField(
                                 decoration:
-                                    InputDecoration(labelText: 'توضیحات'),
+                                InputDecoration(labelText: 'تاریخ ثبت'),
+                                onSaved: (val) => setState(
+                                        () => _addProduct.recordTime = val),
                                 keyboardType: TextInputType.multiline,
-                                maxLines: 5,
+                              )
+                          )),
+//
+//
+//                      SizedBox(
+//                        height: 20.0,
+//                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.center,
+                            child: image == null
+                                ? Text('No image selected.')
+                                : Image.file(image, width: 200, height: 200, fit: BoxFit.cover),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 60.0),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.camera,
+                                size: 30.0,
+                              ),
+                              onPressed: () {
+                                getImage();
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+
+
+
+                      Padding(
+                          padding: EdgeInsets.all(12),
+                          child: new Directionality(
+                              textDirection: TextDirection.rtl,
+                              child: TextFormField(
+                                decoration:
+                                InputDecoration(labelText: 'توضیحات'),
+                                onSaved: (val) => setState(
+                                        () => _addProduct.description = val),
+                                keyboardType: TextInputType.multiline,
+                                maxLines: 4,
                               ))),
                       Padding(
                           padding: EdgeInsets.all(12),
                           child: RaisedButton(
                               color: Color(0xff33CCFF),
                               child: Text("تایید"),
-                              onPressed: () {}))
+                              onPressed: () {
+                                final form = _formKey.currentState;
+                                if (form.validate()) {
+                                  form.save();
+                                  return _makePostRequest(setState,context);
+                                }
+                              })),
+//                      Padding(
+//                          padding: EdgeInsets.all(12),
+//                          child: RaisedButton(
+//                              color: Color(0xff33CCFF),
+//                              child: Text("login"),
+//                              onPressed: () async {
+//                                Map<String, String> headers = {"Content-type": "application/json"};
+//                                final response= await http.post('http://198.143.182.157/api/Salesmans/Login',headers: headers, body:{"userName":"username","password":123});
+//                                Map<String, dynamic> mapdecode = json.decode(response.body);
+//                               cookie= mapdecode['tooken'];
+//                                })),
+//                      Padding(
+//                          padding: EdgeInsets.all(12),
+//                          child: RaisedButton(
+//                              color: Color(0xff33CCFF),
+//                              child: Text("username"),
+//                              onPressed: () async {
+//                                Map<String, String> headers = {"Content-type": "application/json","Authorization":"Bearer $cookie"};
+//                                final response= await http.post('http://198.143.182.157/api/Salesmans/Login',headers: headers, body:{"userName":"username","password":123});
+//                                Map<String, dynamic> mapdecode = json.decode(response.body);
+//                                cookie= mapdecode['tooken'];
+//                              }))
                     ]),
                   ),
                 ],
@@ -141,5 +256,29 @@ class _AddProductState extends State<AddProduct> {
             ),
           ),
         ));
+  }
+
+//  _showDialoge(BuildContext context) {
+//    Scaffold.of(context).showSnackBar(SnackBar(content: Text('submittt')));
+//  }
+
+  Future _makePostRequest(StateSetter state,BuildContext context) async {
+    try {
+      Map<String, String> headers = {"Content-type": "application/json"};
+      String jsonPost=jsonEncode(_addProduct);
+
+      final response =
+      await http.post('http://198.143.182.157/api/Products/Insert',headers: headers, body:jsonPost
+      ).then((http.Response response) {
+        print(_addProduct.productImages);
+
+      }).catchError((onError) {
+        print("Error: $onError");
+      });
+
+    } catch (e) {
+      print("Exception Caught: $e");
+    }
+
   }
 }
